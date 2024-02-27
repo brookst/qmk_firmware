a8bf5769be9fe8c79f4d04420d1939d6fb201d55
Tim Brooks <brooks@skoorb.net>
2024-02-27 23:03:31 +0000

    Rework brackets and add some keys

--- keyboards/lily58/keymaps/brookst/keymap.c
+++ keyboards/lily58/keymaps/brookst/keymap.c
@@ -10,6 +10,8 @@
 #include "raw_hid.h"
 #endif
 
+#define UK_PIPE S(KC_NUBS)
+
 enum layer_number {
   _QWERTY = 0,
   _GAME,
@@ -73,11 +75,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *         ,-----------------------------------------.                    ,-----------------------------------------.
  *         |      |      |      |      |      |      |                    |      |      | PSCR | SCRL | PAUSE|      |
  *         |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- *         |  `   |   >  |   }  |   ]  |   )  |      |                    |      |   {  |   }  |   [  |   ]  |      |
+ *         |  `   |   <  |   {  |   [  |   (  |      |                    |      |   {  |   }  |   [  |   ]  |      |
  *         |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- *         |      |   <  |   {  |   [  |   (  |      |-------.    ,-------| Left | Down |  Up  | Right|   :  |  =   |
- *         |------+------+------+------+------+------|  LGUI |    | RAlt  |------+------+------+------+------+------|
- *         |      |      |      |      |      |      |-------|    |-------| Home | PgDn | PgUp |  End |Insert|      |
+ *         |      |   >  |   }  |   ]  |   )  |      |-------.    ,-------| Left | Down |  Up  | Right|   :  |  =   |
+ *         |------+------+------+------+------+------|  LGUI |    | RGUI  |------+------+------+------+------+------|
+ *         |      |   |  |      |      |      |      |-------|    |-------| Home | PgDn | PgUp |  End |Insert|      |
  *         `-----------------------------------------/       /     \      \-----------------------------------------'
  *                           | LGUI | LAlt |LOWER | / Space /       \ Enter\  |RAISE |Delete| RCtl |
  *                           |      |      |      |/       /         \      \ |      |      |      |
@@ -86,16 +88,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                   _______, _______, KC_PSCR, KC_SCRL, KC_PAUS, _______,
-   KC_GRV, KC_RABK, KC_RCBR, KC_RBRC, KC_RPRN, _______,                   _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
-  _______, KC_LABK, KC_LCBR, KC_LBRC, KC_LPRN, _______,                   KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_COLN,  KC_EQL,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,  KC_INS, _______,
+   KC_GRV, KC_LABK, KC_LCBR, KC_LBRC, KC_LPRN, _______,                   _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
+  _______, KC_RABK, KC_RCBR, KC_RBRC, KC_RPRN, _______,                   KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_COLN,  KC_EQL,
+  _______, UK_PIPE, _______, _______, _______, _______, _______, KC_RGUI, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,  KC_INS, _______,
                              _______, _______, _______, _______, _______, _______,  KC_DEL, _______
 ),
 /* ADJUST
  *         ,-----------------------------------------.                    ,-----------------------------------------.
  *         | NKRO |      |      |      |      | GAME |                    |      |      |      |      |      | DBG  |
  *         |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- *         |      |      |      |      |      |      |                    | FFwd |      |      |  Rwd |      |      |
+ *         |Console      |      |      |      | Txt  |                    | FFwd |      |      |  Rwd |      |      |
  *         |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  *         |      | BBack| BRef | BStop| BFwd |      |-------.    ,-------| Prev | Play | Stop | Next |      |      |
  *         |------+------+------+------+------+------| OLED- |    | OLED+ |------+------+------+------+------+------|
@@ -107,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   [_ADJUST] = LAYOUT(
   NK_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,TG(_GAME),                  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DB_TOGG,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_MFFD, XXXXXXX, XXXXXXX, KC_MRWD, XXXXXXX, XXXXXXX,
+  G(KC_GRV),XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX,LSG(KC_T),                  KC_MFFD, XXXXXXX, XXXXXXX, KC_MRWD, XXXXXXX, XXXXXXX,
   XXXXXXX, KC_WBAK, KC_WREF, KC_WSTP, KC_WFWD, XXXXXXX,                   KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, KC_CALC, XXXXXXX, XXXXXXX,  KC_BDN,  KC_BUP, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX,
                              _______, _______, _______, _______, _______, _______, _______, _______
@@ -122,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *         |------+------+------+------+------+------|   G   |    |       |------+------+------+------+------+------|
  *         |      | Ctrl |  Z   |  X   |  C   |  V   |-------|    |-------|      |      |      |      |      |      |
  *         `-----------------------------------------/       /     \      \-----------------------------------------'
- *                           | LGUI |Enter |INVENT| / Space /       \ Enter\  |RAISE |BackSP| RCtl |
+ *                           |BackSP|Enter |INVENT| / Space /       \ Enter\  |RAISE |BackSP| RCtl |
  *                           |      |      |      |/       /         \      \ |      |      |      |
  *                           `----------------------------'           `------`'--------------------'
  */
@@ -131,19 +133,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_T,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,                   _______, _______, _______, _______, _______, _______,
      KC_H, KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,                   _______, _______, _______, _______, _______, _______,
   _______, KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_G, _______, _______, _______, _______, _______, _______, _______,
-                             _______, KC_ENT,MO(_INVENT),KC_SPC, _______, _______, _______, _______
+                             KC_BSPC, KC_ENT,MO(_INVENT),KC_SPC, _______, _______, _______, _______
 ),
 /* INVENTORY
  *         ,-----------------------------------------.                    ,-----------------------------------------.
  *         | F12  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  *         |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- *         |  `   |   1  |   2  |   3  |   4  |   5  |                    |      |      |      |      |      |      |
+ *         |  `   |  1   |  2   |  3   |  4   |  5   |                    |      |      |      |      |      |      |
  *         |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- *         |      |   6  |   7  |   8  |   9  |   0  |-------.    ,-------|      |      |      |      |      |      |
+ *         |      |  6   |  7   |  8   |  9   |  0   |-------.    ,-------|      |      |      |      |      |      |
  *         |------+------+------+------+------+------|  GAME |    |       |------+------+------+------+------+------|
- *         |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
+ *         |      |  Y   |  J   |  B   |  N   |  M   |-------|    |-------|      |      |      |      |      |      |
  *         `-----------------------------------------/       /     \      \-----------------------------------------'
- *                           | LGUI |Enter |INVENT| / Space /       \ Enter\  |RAISE |BackSP| RCtl |
+ *                           |BackSP|Enter |INVENT| / Space /       \ Enter\  |RAISE |BackSP| RCtl |
  *                           |      |      |      |/       /         \      \ |      |      |      |
  *                           `----------------------------'           `------`'--------------------'
  */
@@ -151,7 +153,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
    KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   _______, _______, _______, _______, _______, _______,
   _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                   _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______,TG(_GAME),_______, _______, _______, _______, _______, _______, _______,
+  _______,    KC_Y,    KC_J,    KC_B,    KC_N,    KC_M,TG(_GAME),_______, _______, _______, _______, _______, _______, _______,
                              _______, _______, _______, _______, _______, _______, _______, _______
   )
 };
